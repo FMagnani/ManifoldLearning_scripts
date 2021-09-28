5480df2f356e24a0415d02b6c4efed54540e847e
FMagnani <federico.magnani5@studio.unibo.it>
2021-09-28 15:33:27 +0200

    Changed slightly plotting, moved scripts in test dir

--- basic_LaplacianEigenmap.py
+++ basic_LaplacianEigenmap.py
@@ -12,7 +12,6 @@ import numpy as np
 from sklearn.manifold import spectral_embedding
 import matplotlib.pyplot as plt
 
-#%%
 
 # OK FOR SWISS ROLL
 # NOT YET READY FOR MNIST
@@ -62,155 +61,40 @@ class basic_LapEig():
         return weighted_graph
 
 
-    def plot(self, ax, dims=2):
-        
-        # 2D plot
-        if (dims==2):
-            
-            x = self.embedding[:,0]
-            y = self.embedding[:,1]
+    def plot_2D(self, ax):
+
+        x = self.embedding[:,0]
+        y = self.embedding[:,1]
                         
-            pars_legend = 'n_neighbors: '+str(self.n_neighbors)+\
-                            '\nt: '+str(self.t)   
-            ax.annotate(pars_legend, xy=(0.75,0.05),xycoords='axes fraction')
+        pars_legend = 'n_neighbors: '+str(self.n_neighbors)+\
+                        '\nt: '+str(self.t)   
+        ax.annotate(pars_legend, xy=(0.75,0.05),xycoords='axes fraction')
             
-            ax.scatter(x,y, c=self.labels)
+        ax.scatter(x,y, c=self.labels)
             
-            return ax
+        return ax
             
-        #3D plot
-        if (dims==3):
+    def plot_3D(self):
             
-            ax = fig.add_subplot(projection = '3d')
+        fig = plt.figure()
+        ax = fig.add_subplot(projection = '3d')
         
-            x = self.embedding[:,0]
-            y = self.embedding[:,1]
-            z = self.embedding[:,2]
+        x = self.embedding[:,0]
+        y = self.embedding[:,1]
+        z = self.embedding[:,2]
         
-            title = 'Laplacian Eigenmap embedding\nknn: '+\
-                    str(self.n_neighbors)+', t: '+str(self.t)
+        title = 'Laplacian Eigenmap embedding\nknn: '+\
+                str(self.n_neighbors)+', t: '+str(self.t)
         
-            ax.set_title(title)
-            ax.set_xlabel('First eigenvector')
-            ax.set_ylabel('Second eigenvector')
-            ax.set_zlabel('Third eigenvector')
+        ax.set_title(title)
+        ax.set_xlabel('First eigenvector')
+        ax.set_ylabel('Second eigenvector')
+        ax.set_zlabel('Third eigenvector')
             
-            ax.scatter(x,y,z, c = self.labels, cmap = 'viridis')        
+        ax.scatter(x,y,z, c = self.labels, cmap = 'viridis')        
+                        
             
-            return ax
-            
-            
-            
-#%%
-
-# SCRIPTS
-
-from utils import swiss_roll
-from time import time
-from sklearn.manifold import SpectralEmbedding as SE
-
-scale = 5
-data = swiss_roll(600, 0, 123456, scale)
-
-#%%
-
-# Time change
-
-fig, ax = plt.subplots(2,2)
-
-fig.suptitle("Laplacian Eigenmap embedding")
-
-times = [5, 33, 66, 100]
-ax_index = [(0,0),(0,1),(1,0),(1,1)]
-
-start = time()
-for (t,axis) in zip(times, ax_index):
-    
-    emb = basic_LapEig(data.data, data.t, 15, 2, t)
-    
-    i = axis[0]
-    j = axis[1]
-    ax[i,j] = emb.plot(ax[i,j], 2)
-
-stop = time()
-print("time to run: "+str(stop-start))
-
-fig.show()        
-
-
-#%%
-
-# N_Neighbors change
-
-fig, ax = plt.subplots(2,2)
-
-fig.suptitle("Laplacian Eigenmap embedding")
-
-knn = [5, 15, 20, 30]
-ax_index = [(0,0),(0,1),(1,0),(1,1)]
-
-start = time()
-for (knn,axis) in zip(knn, ax_index):
-    
-    emb = basic_LapEig(data.data, data.t, knn, 2, 30)
-    
-    i = axis[0]
-    j = axis[1]
-    ax[i,j] = emb.plot(ax[i,j], 2)
-
-stop = time()
-print("time to run: "+str(stop-start))
-
-fig.show()        
-
-#%%
-
-# basic_LapEig VS sklearn.manifold.SpectralEmbedding
-
-# N_Neighbors change
-
-fig, ax = plt.subplots(3,2)
-
-fig.suptitle("My implementation VS sklearn implementation")
-
-knn = [5, 15, 25]
-ax_index_mine = [(0,0),(1,0),(2,0)]
-ax_index_skl = [(0,1),(1,1),(2,1)]
-
-my_time = scale*scale*100
-
-start = time()
-
-for (n_neighbors,axis) in zip(knn, ax_index_mine):
-    
-    emb = basic_LapEig(data.data, data.t, n_neighbors, 2, 2500)
-    
-    i = axis[0]
-    j = axis[1]
-    ax[i,j] = emb.plot(ax[i,j], 2)
-
-for (n_neighbors,axis) in zip(knn, ax_index_skl):
-    
-    emb = SE(n_components=2, n_neighbors=n_neighbors).fit_transform(data.data)
-    
-    i = axis[0]
-    j = axis[1]
-    
-    ax[i,j].scatter( emb[:,0], emb[:,1], c = data.t )
-    
-    pars_legend = 'n_neighbors: '+str(n_neighbors)+\
-                            '\nt: inf'   
-    ax[i,j].annotate(pars_legend, xy=(0.75,0.05),xycoords='axes fraction')
-           
-
-
-stop = time()
-print("time to run: "+str(stop-start))
-
-fig.show()        
-
-
-
+        
 
 
 
